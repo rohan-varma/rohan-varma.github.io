9c0d4f931a951df8937d8cee05772a810ec76e16
Rohan Varma <rvarm1@ucla.edu>
2019-02-08 02:11:49 -0800

    Update angular.js

--- js/angular.js
+++ js/angular.js
@@ -7,26 +7,7 @@ myApp.controller("myController", function($scope){
             name: "Home",
             link: "index.html"
         },
-        {
-            name: "Blog",
-            link: "rohan-blog"
-        },
-        {
-            name: "About Me",
-            link: "about.html"
-        },
-        {
-            name: "Projects",
-            link: "projects.html"
-        },
-        {
-            name: "Research",
-            link: "research.html"
-        },
-        {
-            name: "Contact",
-            link: "contact.html"
-        },
+        
     ];
     
     $scope.getOptions=function(){
